6efb2d8ba4d4bad237576520c67e9b3d8942ec1c
Ed-Perez-G <53677711+Ed-Perez-G@users.noreply.github.com>
2026-08-26 16:14:09 -0400

    Ejercicios dplyt parte 2

--- Scripts_Problemas.R
+++ Scripts_Problemas.R
@@ -102,15 +102,210 @@ df |>
 ########### if_else() ########### 
 
 
+df |>
+  mutate(
+    GrupoEdad = if_else(
+      Age >= 18,
+      "Adulto",
+      "Menor"
+    )
+  )
+
+df <-  df |> 
+  mutate(NuevoGrupo =
+  ifelse(
+    Gender == "female" & Age >25, 
+    "Grupo A", 
+    "Grupo B"
+  )) 
+
+# Pacientes con T-LGLL O con RA asociada
+dplyr::if_else(
+  diagnostico == "T-LGLL" | diagnostico == "RA",
+  "Sí",
+  "No"
+)
+
 ########### case_when() ########### 
+
+df |>
+  mutate(
+    CategoriaBMI = case_when(
+      is.na(BMI) ~ "Sin información",
+      BMI < 18.5 ~ "Bajo peso",
+      BMI < 25 ~ "Peso esperado",
+      BMI < 30 ~ "Sobrepeso",
+      BMI >= 30 ~ "Obesidad"
+    )
+  )
 
 
 ########### transmute() ########### 
 
+df_obesidad <- df |>
+  transmute(
+    Age,
+    Sex,
+    BMI,
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      Age >= 65 ~ "65 o más"
+    ),
+    Obesidad = case_when(
+      BMI >= 30 ~ "Sí",
+      BMI < 30 ~ "No"
+    )
+  )
+
+
+###### problema 2
+
+df_presion <- df |>
+  transmute(
+    Age,
+    Sex,
+    BPSysAve,
+    BPDiaAve,
+    PAM = (BPSysAve + 2 * BPDiaAve) / 3,
+    Hipertension = case_when(
+      BPSysAve >= 140 | BPDiaAve >= 90 ~ "Sí",
+      TRUE ~ "No" # Para evitar poner toda la condicion
+    )
+  )
 
 ########### group_by() ########### 
 
+df |>
+  group_by(Sex) |>
+  summarise(
+    BMI_promedio = mean(BMI, na.rm = TRUE)
+  )
+
+df |>
+  mutate(
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      TRUE ~ "65 o más"
+    )
+  ) |>
+  group_by(GrupoEdad) |>
+  summarise(
+    n = n(),
+    Edad_promedio = mean(Age, na.rm = TRUE)
+  )
+
+# Problema 3. 
+
+# Queremos explorar si el BMI y la presión arterial sistólica difieren según el sexo y el grupo de edad. Además, queremos calcular la proporción de participantes con obesidad dentro de cada grupo.
+
+df |>
+  mutate(
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      TRUE ~ "65 o más"
+    ),
+    Obesidad = BMI >= 30
+  ) |>
+  group_by(Sex, GrupoEdad) |>
+  summarise(
+    n = n(),
+    BMI_promedio = mean(BMI, na.rm = TRUE),
+    BMI_mediana = median(BMI, na.rm = TRUE),
+    PAS_promedio = mean(BPSysAve, na.rm = TRUE),
+    Obesidad_pct = mean(Obesidad, na.rm = TRUE) * 100,
+    .groups = "drop"
+  )
+
+
+
+
+df |>
+  mutate(
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      TRUE ~ "65 o más"
+    ),
+    Obesidad = BMI >= 30
+  ) |>
+  group_by(Gender, GrupoEdad) |>
+  summarise(
+    n = n(),
+    BMI_promedio = mean(BMI, na.rm = TRUE),
+    BMI_mediana = median(BMI, na.rm = TRUE),
+    PAS_promedio = mean(BPSysAve, na.rm = TRUE),
+    Obesidad_pct = mean(Obesidad, na.rm = TRUE) * 100,
+    .groups = "drop"
+  )
+
+
+
+## Problema
+
+df |> 
+  mutate(
+    GrupoEdad = # Varible 1
+      case_when(
+        Age < 18 ~ "Menos de 18",
+        Age <65 ~ "Edad 2", 
+        TRUE ~ "Mas de 65"),
+    Obesidad = BMI >= 30) |> # Variable 2 solo responde si  y no
+  group_by(Gender, GrupoEdad) |> 
+  summarise(
+    n = n(), 
+    "Promedio BMI" = mean(BMI, na.rm= T), 
+    "Mediana BMI" = median(BMI, na.rm = T),
+    "Promedio presion" = mean(BPSysAve, na.rm = T),
+    "Porcentaje obesidad" = mean(Obesidad, na.rm = T)*100, 
+.groups = "drop") # desgrupa
+
+
+
+df |>
+  mutate(
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      TRUE ~ "65 o más"
+    ),
+    Obesidad = case_when(
+      BMI >= 30 ~ "Sí",
+      BMI < 30 ~ "No"
+    )
+  ) |>
+  group_by(Gender, GrupoEdad) |>
+  summarise(
+    n = n(),
+    BMI_promedio = mean(BMI, na.rm = TRUE),
+    BMI_mediana = median(BMI, na.rm = TRUE),
+    PAS_promedio = mean(BPSysAve, na.rm = TRUE),
+    Obesidad_pct = sum(Obesidad == "Sí", na.rm = TRUE) /
+      sum(!is.na(Obesidad)) * 100,
+    .groups = "drop"
+  )
 
 ########### summarise() ########### 
+
+df |>
+  mutate(
+    GrupoEdad = case_when(
+      Age < 18 ~ "Menor de 18",
+      Age < 65 ~ "18 a 64",
+      TRUE ~ "65 o más"
+    )
+  ) |>
+  group_by(GrupoEdad) |>
+  summarise(
+    n = n(),
+    Colesterol_promedio = mean(TotChol, na.rm = TRUE),
+    Colesterol_DE = sd(TotChol, na.rm = TRUE),
+    Fumadores = sum(SmokeNow == "Yes", na.rm = TRUE),
+    Fumadores_pct = sum(SmokeNow == "Yes", na.rm = TRUE) /
+      sum(!is.na(SmokeNow)) * 100,
+    .groups = "drop"
+  )
 
 ########### across() ########### 
